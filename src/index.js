1a75dd82b2d8d4597a6fe085ad3095992b75d90f
Zoran Obradović <zockyo@gmail.com>
2019-10-27 21:31:51 +0100

    init

--- src/index.js
+++ src/index.js
@@ -1,41 +1,37 @@
 import { WebShellApp } from "@websh/web-shell-app";
 import manifest from "./manifest.json";
 
-console.log(manifest)
+WebShellApp.manifest(manifest);
+import Quill from "quill"
+import "quill/dist/quill.snow.css";
 
-
-
-delete window.sessionStorage ;
-
-var data1 = [
-  [ 'Cheese', 10, 1.10, '=B1*C1'],
-  [ 'Apples', 30, 0.40, '=B2*C2'],
-  [ 'Carrots', 15, 0.45, '=B3*C3'],
-  [ 'Oranges', 20, 0.49, '=B4*C4'],
-];
-
-window.table = jexcel(document.getElementById('editor'), {
-  data:data1,
-  colHeaders: [ 'Product', 'Quantity', 'Price', 'Total' ],
-  colWidths: [ 300, 100, 100, 100 ],
-  columns: [
-      { type: 'autocomplete', source:[ 'Apples','Bananas','Carrots','Oranges','Cheese','Pears' ] },
-      { type: 'number' },
-      { type: 'number' },
-      { type: 'number' },
-  ]
+var editor = new Quill('#editor', {
+  theme: 'snow'
 });
 
+WebShellApp.command('file-open',({format,content,extension})=>{
+  switch (format) {
+    case "quill": 
+      editor.setContents(JSON.parse(content));
+      break;
+    case "text":
+      editor.setText(content);
+      break;
+  }
+})
 
-WebShellApp.manifest(manifest)
-.command('file-open', ({ format, content, type, extension }) => {
-  return "OK";
+WebShellApp.command('file-save',({format})=>{
+  switch (format) {
+    case "quill": 
+      return {content: JSON.stringify(editor.getContents())}
+    case "text":
+      return {content: editor.getText()}
+    case "html":
+        return {content: document.getElementById("editor").firstElementChild.innerHTML}
+    }
+  
 })
-.command('file-save', ({ format, extension }) => {
-  return {}
-})
-.command('file-new', function ({ format }) {
-  console.log({table})
-  table.setData([])
-  return "OK";
+
+WebShellApp.command('file-new',()=>{
+  editor.setText("")
 })
